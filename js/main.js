c381195f2ceb8f814dc587e70163274d969fc53a
tony <tonytenorioc@gmail.com>
2019-04-06 15:10:55 -0600

    Adding typing effect

--- js/main.js
+++ js/main.js
@@ -5,6 +5,17 @@ $(document).ready(function() {
         perturbance: 0.01
     });
 
+    var typed = new Typed('.typed', {
+        strings: ["<strong class='primary'>SPARKY.</strong> ^2000", "<strong class='secondary'>Awesome Developers.</strong>^2000"],
+        smartBackspace: false,
+        loop: true,
+        showCursor:true,
+        cursorChar:"|",
+        typeSpeed: 50,
+        backSpeed: 30,
+        startDelay:800
+      });
+
 
     $('#date').text(new Date().getFullYear());
 });
